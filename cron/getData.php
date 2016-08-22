a4cf2bc4c5f646bad69b71744f18460e7c26c974
zhaojipeng <zhaojipeng@haodai.com>
2016-08-22 18:28:14 +0800

    获取股票信息

--- cron/getData.php
+++ cron/getData.php
@@ -7,8 +7,6 @@
  */
 
 
-http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=100&sort=symbol&asc=1&node=hs_a&symbol=&_s_r_a=init
-
 /**
  * 数据含义
  * [0] => 柳钢股份           名称
